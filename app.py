af370d4ee56bc9a54702ba197a45cad3fc213bfd
Maksim Hakimov <hakmaks@yandex.ru>
2022-11-29 17:11:38 +0200

    сделал позиционирование формам ввода + добавил кнопку и контент

--- app.py
+++ app.py
@@ -6,24 +6,36 @@ template_folder = 'templates'
 app = Flask(__name__)
 
 
+# Главна страница сайта
 @app.route('/')
 def index():
+    """Передаю в переменную последнее введенное сообщение из функции загрузки json
+       сохраняю список json в переменную
+    """
     text = load_json()[-1].get('text')
     wish_list = load_json()
 
     if len(wish_list) <= 10:
         texts = wish_list
     else:
+        # печатаю последние 10 сообщений из списка словарей в переменную
         texts = wish_list[:-10: -1]
 
+        # Возвращаю путь к файлу html и назначаю переменные, которые буду использовать в html
     return render_template('index.html', text=text, texts=texts)
 
 
+# Страница, передающая введенные пользователем данные
 @app.route('/new_congratulation', methods=['POST'])
 def new_congratulation():
+    """
+    user - принимает введенное пользователем значение, в поле user, с сайта
+    text - принимает введенное пользователем значение, в поле text, с сайта
+    """
     user = request.form.get('user_name')
     text = request.form.get('congratulation')
 
+    # Вызываю функцию, которая добавляет новые данные в словарь json,
     create_dict(user, text)
     return redirect('/', code=302)
 
--- utils.py
+++ utils.py
@@ -2,11 +2,17 @@ import json
 
 
 def load_json():
+    """
+    Загрузка json
+    """
     with open('data/123.json', 'r', encoding='UTF-8') as name_json:
         return json.load(name_json)
 
 
 def create_dict(user, text):
+    """
+    Создает словарь из json. Получает новые значения и добавляет их в созданный словарь
+    """
     dict_one = load_json()
 
     all_dict = {'user': user, 'text': text}
@@ -17,6 +23,9 @@ def create_dict(user, text):
 
 
 def wright_json(dict_one):
+    """
+    Перезаписывает json
+    """
     with open('data/123.json', 'w', encoding='UTF-8') as name_json:
         json.dump(dict_one, name_json, ensure_ascii=False)
     return
